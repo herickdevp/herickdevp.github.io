d2ed25d8556828aab33c6347e349de2ee4148b1f
Herick Silva <herick610@gmail.com>
2023-03-22 15:05:52 -0300

    implementação menu mobile

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+const btnMenu = document.querySelector(".btn-menu");
+
+function toggleMenu(event) {
+  const menuMobile = document.querySelector(".menuMobile");
+  menuMobile.classList.toggle("ativo");
+}
+
+btnMenu.addEventListener("click", toggleMenu);
